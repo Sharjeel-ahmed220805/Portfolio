d67870eb1cf49999b4459b1f89ef0856e49aa5ce
Sharjeel Ahmed <159823455+Sharjeel-ahmed220805@users.noreply.github.com>
2024-11-09 17:35:07 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       <div className='flex justify-between items-center'>
         <div className='text-xl font-medium'> Sharjeel Ahmed</div>
         <ul className='gap-10 lg:gap-16 hidden md:flex'>
-            <li className='menuLink'><a href='#home'>Home</a></li>
+            <li className='menuLink'><a href='#hero'>Home</a></li>
             <li className='menuLink'><a href='#about'>About</a></li>
             <li className='menuLink'><a href='#projects'>Projects</a></li>
             <li className='menuLink'><a href='#skills'>Skills</a></li>
