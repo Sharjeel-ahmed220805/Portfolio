3513a6959e00af8852ed8b722aa1dda13bf9556b
Sharjeel Ahmed <159823455+Sharjeel-ahmed220805@users.noreply.github.com>
2024-11-09 17:23:24 +0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import Navbar from './Navbar'
 
 const Hero = () => {
   return (
-    <div data-aos="flip-left" id='hero'className='min-h-screen bg-no-repeat bg-[url(/powder_blue.png)]  bg-cover'
+    <div data-aos="flip-left" id="hero"className="min-h-screen bg-no-repeat bg-[url(/powder_blue.png)]  bg-cover"
     style={{backgroundSize: "30%", backgroundPosition: "left 100px top 150px"}}
     >
 
@@ -15,9 +15,9 @@ const Hero = () => {
 
 
      {/* serious code*/}
-     <div className='container grid lg:grid-cols-2 h-[calc(100vh-60px)]'>
-      <div className='hidden lg:block'></div>
-      <div className='text[70px] sm:text-[90px] font-bold leading-tight flex justify-end  items-center'>
+     <div className="container grid lg:grid-cols-2 h-[calc(100vh-60px)]">
+      <div className="hidden lg:block"></div>
+      <div className="text[70px] sm:text-[90px] font-bold leading-tight flex justify-end  items-center">
         <div>
           <p data-aos="flip-right">I'm  </p>
           <p data-aos="flip-right">Sharjeel </p>
